bbfeb7ce73e702c0d00b78427eee00329b860600
Adunda13 <64tokyo@gmail.com>
2022-03-30 18:33:52 -0700

    updated lesson three

--- Lesson Three/script.js
+++ Lesson Three/script.js
@@ -1,1 +1,25 @@
 
+import MacBook from "./Macbook.js";
+
+const markup = (Macbook) => {
+    return `
+    <div class="container">
+    <div class="row">
+    <div class="col-lg">
+        <h3>${Macbook.name}</h3>
+        <ul>
+        <li>Size:${Macbook.size}</li>
+        <li>Color:${Macbook.color}</li>
+        <li>VPN:${Macbook.apps.vpn}</li>
+        <li>Media:${Macbook.apps.media}</li>
+        <li>Browser:${Macbook.apps.browser}</li>
+        <li>Photo Editor:${Macbook.apps.designer}</li>
+        <li>Text Editor:${Macbook.apps.textEditor}</li>
+        <li>Lid:${Macbook.lidOpen ? "Open" : "Closed"}</li>
+        </ul>
+    </div> </div></div>   `
+}
+
+const main = document.createElement("main");
+main.innerHTML = markup(MacBook);
+document.body.appendChild(main);
